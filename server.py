dbfed5e8f312dd8d82707bf670208b6329add622
Tom Diggelmann <tdiggelm@gmail.com>
2014-05-28 13:51:10 +0200

    POST expects json

--- server.py
+++ server.py
@@ -23,29 +23,24 @@ def format_result(fp):
 @app.route('/fingerprint/', defaults={'url': ''}, methods=['GET', 'POST'])
 @app.route('/fingerprint/<path:url>', methods=['GET', 'POST'])
 def fingerprint(url=None):
-    args = dict(request.args.items())
-    preserve_entities = str_to_bool(args.pop("preserve_entities", "false"))
-    corpus = args.pop("corpus", "brown").lower().strip()
-    limit = int(args.pop("limit", "10"))
+    if request.method == 'POST':
+        args = request.json
+    else:
+        args = dict(request.args.items())
+        args["preserve_entities"] = str_to_bool(
+            args.get("preserve_entities", "false"))
+        args["corpus"] = args.pop("corpus", "brown").lower().strip()
+        args["limit"] = int(args.pop("limit", "10"))
 
-    if len(args) > 0:
-        raise TypeError("unsupported parameters in querystring")
+    #if len(args) > 0:
+    #    raise TypeError("unsupported parameters in querystring")
     
     if request.method == 'POST':
-        if request.headers.get('content-type').startswith('text/plain'):
-            text = request.get_data().decode('utf-8')
-            
-            fp = keywords_from_text(text, 
-                preserve_entities=preserve_entities, corpus=corpus, 
-                associations_per_keyword=0, limit=limit)
-            return format_result(fp)
-        else:
-            raise BadRequest('expected text/plain, got %s' % request.headers.get('content-type', '<n/a>'))
+        fp = keywords_from_text(associations_per_keyword=0, **args)
     else:
-        fp = keywords_from_url(url, 
-            preserve_entities=preserve_entities, corpus=corpus, 
-            associations_per_keyword=0, limit=limit)
-        return format_result(fp)
+        fp = keywords_from_url(url, associations_per_keyword=0, **args)
+
+    return format_result(fp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
